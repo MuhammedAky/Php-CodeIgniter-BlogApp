f841c37b5eea7329130e6c5cd2bdc18655da4ebc
MuhammedAky <muhammedaky24@gmail.com>
2022-07-14 22:12:24 +0300

    First posts viewed

---
+++ application/views/templates/header.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<title>ciBlog</title>
+		<!-- CSS only -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="<?php echo base_url(); ?>">ciBLog</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo base_url(); ?>">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+					</li class="nav-item">
+					<li>
+						<a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
+					</li>
+			</ul>
+			</div>
+		</div>
+		</nav>
+
+	<div class="container">
