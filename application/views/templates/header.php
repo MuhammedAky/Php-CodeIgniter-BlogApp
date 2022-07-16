65f6ea6d3fa76b1d19f0e7f5703a710d21230efb
MuhammedAky <muhammedaky24@gmail.com>
2022-07-16 12:01:01 +0300

    Create,delete,update post functionalities added

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,7 @@
 		<!-- CSS only -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+		<script src="http://cdn.ckeditor.com/4.5.11/standard/ckeditor.js"></script>
 	</head>
 	<body>
 		<nav class="navbar navbar-expand-lg bg-light">
@@ -23,6 +24,11 @@
 					<li>
 						<a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
 					</li>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a>
+						</li>
+					</ul>
 			</ul>
 			</div>
 		</div>
